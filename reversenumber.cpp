ab892f33b764821b27158125136de72fececf8c1
Lak5hay <tlakshay98@gmail.com>
2024-10-24 01:37:40 +0530

    Create reversenumber.cpp

---
+++ reversenumber.cpp
@@ -0,0 +1,20 @@
+/*lakshay tanwar
+23070123079*/
+#include <iostream>
+using namespace std;
+int main() {
+     int prn,num;
+     cout << "Enter The Number" << endl;
+     cin >> prn;
+     
+     while(prn > 0){
+         num = prn % 10;
+         prn = prn / 10;
+         cout << num;
+     }
+     return 0;
+}
+    // output:
+    //Enter The Number
+//420840
+//048024
